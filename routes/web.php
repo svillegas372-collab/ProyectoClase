3c942707ee8f712c702d0c3310ccd2215c8256c6
CSU22-AINF <CSU22@academico.unab.ad>
2026-02-19 11:08:45 -0500

    Grupo de Rutas

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-Route::prefix('product') ->group(function (){
+Route::prefix('product') ->controller(ProductController::class)->group(function (){
 
 
-Route::get('/product', [ProductController::class, 'index']);
-
-Route::get('/product/create', [ProductController::class, 'create']);
-
-Route::get('/product/{producto}', [ProductController::class, 'show']);
+Route::get('/', 'index');
+Route::get('/create',  'create');
+Route::get('/{producto}', 'show');
 
 });
