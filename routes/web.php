dc961db26c9313bfa55014de146a62e5a5eae50d
CSU22-AINF <CSU22@academico.unab.ad>
2026-02-19 11:06:34 -0500

    Rutas y controlador basico

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
+Route::prefix('product') ->group(function (){
+
+
 Route::get('/product', [ProductController::class, 'index']);
 
 Route::get('/product/create', [ProductController::class, 'create']);
 
 Route::get('/product/{producto}', [ProductController::class, 'show']);
 
+});
